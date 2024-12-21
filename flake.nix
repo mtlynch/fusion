acec83fb2ea25f9be16403afe521ef8aa1c0f6e3
Michael Lynch <git@mtlynch.io>
2024-12-21 12:40:33 -0500

    Fix flake

--- flake.nix
+++ flake.nix
@@ -3,18 +3,14 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-
     # 1.23.2 release
-    # We temporarily mismatch the version in Docker (1.23.3) because Nix doesn't
-    # have the latest version yet, but we need to fix:
-    # https://github.com/golang/go/issues/68976
     go-nixpkgs.url = "github:NixOS/nixpkgs/4ae2e647537bcdbb82265469442713d066675275";
 
     # 3.44.2 release
     sqlite-nixpkgs.url = "github:NixOS/nixpkgs/5ad9903c16126a7d949101687af0aa589b1d7d3d";
 
-    # 20.6.1 release
-    nodejs-nixpkgs.url = "github:NixOS/nixpkgs/78058d810644f5ed276804ce7ea9e82d92bee293";
+    # 22.10.- release
+    nodejs-nixpkgs.url = "github:NixOS/nixpkgs/566e53c2ad750c84f6d31f9ccb9d00f823165550";
 
     # 0.10.0 release
     shellcheck-nixpkgs.url = "github:NixOS/nixpkgs/4ae2e647537bcdbb82265469442713d066675275";
@@ -32,13 +28,15 @@
       gopkg = go-nixpkgs.legacyPackages.${system};
       go = gopkg.go_1_23;
       sqlite = sqlite-nixpkgs.legacyPackages.${system}.sqlite;
-      nodejs = nodejs-nixpkgs.legacyPackages.${system}.nodejs_20;
+      nodejs = nodejs-nixpkgs.legacyPackages.${system}.nodejs_22;
       shellcheck = shellcheck-nixpkgs.legacyPackages.${system}.shellcheck;
+      mockgen = gopkg.mockgen;
+      pkgsMusl = go-nixpkgs.legacyPackages.${system}.pkgsMusl;
     in {
       devShells.default =
         go-nixpkgs.legacyPackages.${system}.mkShell.override
         {
-          stdenv = go-nixpkgs.legacyPackages.${system}.pkgsStatic.stdenv;
+          stdenv = pkgsMusl.stdenv;
         }
         {
           packages = [
@@ -53,10 +51,12 @@
             sqlite
             nodejs
             shellcheck
+            mockgen
+            pkgsMusl.musl
           ];
-
           shellHook = ''
             export GOROOT="${go}/share/go"
+            export CGO_ENABLED=1
 
             echo "shellcheck" "$(shellcheck --version | grep '^version:')"
             echo "node" "$(node --version)"
