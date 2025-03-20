887c4fbed02f5a07bf0d1bf93c3e8fe124a45c31
Michael Lynch <git@mtlynch.io>
2025-03-19 20:35:42 -0400

    Move updateFeedInStore

--- service/pull/handle.go
+++ service/pull/handle.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/0x2e/fusion/model"
+	"github.com/0x2e/fusion/pkg/ptr"
 	"github.com/0x2e/fusion/service/pull/client"
 )
 
@@ -35,6 +36,31 @@ func (p *Puller) do(ctx context.Context, f *model.Feed, force bool) error {
 
 	logger.Infof("fetched feed successfully")
 	return nil
+}
+
+// updateFeedInStore implements UpdateFeedInStoreFn for SingleFeedPuller.
+func (p *Puller) updateFeedInStore(feedID uint, items []*model.Item, lastBuild *time.Time, requestError error) error {
+	if requestError != nil {
+		return p.feedRepo.Update(feedID, &model.Feed{
+			Failure: ptr.To(requestError.Error()),
+		})
+	}
+
+	if len(items) > 0 {
+		// Set the correct feed ID for all items.
+		for _, item := range items {
+			item.FeedID = feedID
+		}
+
+		if err := p.itemRepo.Insert(items); err != nil {
+			return err
+		}
+	}
+
+	return p.feedRepo.Update(feedID, &model.Feed{
+		LastBuild: lastBuild,
+		Failure:   ptr.To(""),
+	})
 }
 
 // FeedUpdateAction represents the action to take when considering checking a
--- service/pull/singlefeed.go
+++ service/pull/singlefeed.go
@@ -5,7 +5,6 @@ import (
 	"time"
 
 	"github.com/0x2e/fusion/model"
-	"github.com/0x2e/fusion/pkg/ptr"
 	"github.com/0x2e/fusion/service/pull/client"
 )
 
@@ -43,29 +42,4 @@ func (p SingleFeedPuller) Pull(ctx context.Context, feed *model.Feed) error {
 	// We don't exit on error, as we want to record any error in the data store.
 	fetchResult, readErr := p.readFeed(ctx, *feed.Link, feed.FeedRequestOptions)
 	return p.updateFeedInStore(feed.ID, fetchResult.Items, fetchResult.LastBuild, readErr)
-}
-
-// updateFeedInStore implements UpdateFeedInStoreFn for SingleFeedPuller.
-func (p *Puller) updateFeedInStore(feedID uint, items []*model.Item, lastBuild *time.Time, requestError error) error {
-	if requestError != nil {
-		return p.feedRepo.Update(feedID, &model.Feed{
-			Failure: ptr.To(requestError.Error()),
-		})
-	}
-
-	if len(items) > 0 {
-		// Set the correct feed ID for all items.
-		for _, item := range items {
-			item.FeedID = feedID
-		}
-
-		if err := p.itemRepo.Insert(items); err != nil {
-			return err
-		}
-	}
-
-	return p.feedRepo.Update(feedID, &model.Feed{
-		LastBuild: lastBuild,
-		Failure:   ptr.To(""),
-	})
 }
